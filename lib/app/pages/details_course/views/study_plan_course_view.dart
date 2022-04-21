5aef587eaa25530d4887f302fea77ce86afe3a6c
ADawoud-Mobile <adawoud.acc@gmail.com>
2022-04-21 12:06:24 +0200

    remove all unnecessary cupertinu libs

--- lib/app/pages/details_course/views/study_plan_course_view.dart
+++ lib/app/pages/details_course/views/study_plan_course_view.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hayah_karema/app/common/managers/api/courses/_models/course_model.dart';
--- lib/app/pages/profile/views/profile_awards_view.dart
+++ lib/app/pages/profile/views/profile_awards_view.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hayah_karema/app/common/managers/api/profile/_model/user_rewards.dart';
