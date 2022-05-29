8917ade852a4ded22c99e801aefc09f688444ca6
ADawoud <adawoud.acc@gmail.com>
2022-05-29 14:54:44 +0200

    remove home  GetMaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,9 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:hayah_karema/app/common/themes/app_theme.dart';
 import 'package:hayah_karema/app/common/translation/app_translations.dart';
-import 'package:hayah_karema/app/pages/profile/_widgets/profile_addresses.dart';
-import 'package:hayah_karema/app/pages/profile/_widgets/profile_payment_cards.dart';
-import 'package:hayah_karema/app/pages/profile/profile_binding.dart';
+import 'package:hayah_karema/app/routes/app_pages.dart';
 import 'package:hayah_karema/setup.dart';
 
-import 'app/routes/app_pages.dart';
 
 void main() async{
   /// setup locator.
@@ -19,18 +16,14 @@ void main() async{
       designSize: const Size(360, 760),
       builder: (cxt, child)=> GetMaterialApp(
         title: "حياة كريمة",
-        // initialRoute: AppPages.INITIAL,
-        // getPages: AppPages.routes,
+        initialRoute: AppPages.INITIAL,
+        getPages: AppPages.routes,
         enableLog: BUILD_TYPE == BuildType.debug,
         locale: const Locale('ar'),
         fallbackLocale: const Locale('ar'),
         translations: AppTranslations(),
         theme: AppTheme.defaultTheme,
         debugShowCheckedModeBanner: BUILD_TYPE == BuildType.debug,
-
-        // todo  for test only
-        initialBinding: ProfileBinding(),
-        home: const ProfilePaymentCards(),
       ),
     ),
   );
